6d6a4b6fb32f0093a4ff2d8a070e956440c6a7dd
Paulii <paulamorales@epet12smandes.edu.ar>
2022-12-07 11:29:13 -0300

    Tarea 1 y 2 completadas

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,41 +1,45 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import icon from '../../assets/icon.svg';
+import ciudad from '../../assets/ciudad.jpg';
 import './App.css';
 
 function Home() {
   let titulo = "Home";
 
   return (
+    <div>
     <div className="Home">
-      <img width="200" alt="icon" src={icon} />
-    </div><h1>Home</h1><div className="Home">
-        <a
-          href="https://classroom.google.com"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="blackboard">
-              👨‍🏫
-            </span>
-            Classroom
-          </button>
-        </a>
-        <a
-          href="https://github.com/Moralespaulii/Poec-Morales-Paula.git"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="cat">
-              😺
-            </span>
-            GitHub
-          </button>
-        </a>
-      </div>
-  );
-}
+      <img width="200" alt="ciudad" src={ciudad} />
+    </div>
+    <h1>¡Bienvenido!</h1>
+    <div className="Home">
+      <a
+        href="https://classroom.google.com/u/0/h"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <button type="button">
+          <span role="img" aria-label="blackboard">
+            👨‍🏫
+          </span>
+          Classroom
+        </button>
+      </a>
+      <a
+        href="https://github.com/Moralespaulii/Poec-Morales-Paula"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <button type="button">
+          <span role="img" aria-label="cat">
+            😺
+          </span>
+          GitHub
+        </button>
+      </a>
+    </div>
+  </div>
+);
+};
 
 
 export default function App() {
